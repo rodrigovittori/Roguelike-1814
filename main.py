03c73183429281fa23dd1f36e403227521578ad2
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-07-22 21:54:52 -0300

    Agregar función dibujar_mapa()
    Version actual: [M7.L1: Actividad 3/9]

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #pgzero
 
 """
-Version actual: [M7.L1: Actividad 2/9]
+Version actual: [M7.L1: Actividad 3/9]
 packs de assets: https://kenney.nl/assets/series:Tiny?sort=update
 """
 
-# Ventana de juego hecha de celdas
-celda = Actor('border')
+celda = Actor('border') # Celda de ejemplo (se usa para calcular escalas)
+
+# Paleta de terrenos:
+
+pared =  Actor("border") # 0: Pared de bloques
+piso =   Actor("floor")  # 1: Suelo liso
+crack =  Actor("crack")  # 2: Suelo resquebrajado/quebradizo
+huesos = Actor("bones")  # 3: Suelo con pilita de huesos
+
 size_w = 7 # Ancho del mapa en celdas
 size_h = 7 # Altura del mapa en celdas
 
@@ -23,3 +30,48 @@ mapa = [[0, 0, 0, 0, 0, 0, 0],
         [0, 1, 1, 1, 3, 1, 0],
         [0, 1, 3, 1, 1, 2, 0],
         [0, 0, 0, 0, 0, 0, 0]]
+        
+mapa2 = [[0, 0, 0, 0, 0, 0, 0],
+         [0, 1, 1, 3, 1, 1, 0],
+         [0, 1, 3, 1, 3, 1, 0],
+         [0, 3, 1, 1, 1, 3, 0],
+         [0, 3, 1, 1, 1, 3, 0],
+         [0, 1, 3, 3, 3, 1, 0],
+         [0, 0, 0, 0, 0, 0, 0]]
+
+def dibujar_mapa(mapa):
+    
+    for fila in range(len(mapa)):
+        for columna in range(len(mapa[0])):
+            
+            """
+            0: pared
+            1: piso (sin nada)
+            2: piso (roto/resquebrajado)
+            3: piso (c/ huesitos)
+            """
+            
+            if (mapa[fila][columna] == 0): # pared
+                pared.left = pared.width * fila
+                pared.top =  pared.height * columna
+                pared.draw()
+                
+            elif (mapa[fila][columna] == 1): # piso (sin nada)
+                piso.left = pared.width * fila
+                piso.top =  pared.height * columna
+                piso.draw()
+                
+            elif (mapa[fila][columna] == 2): # piso (roto/resquebrajado)
+                # crack
+                crack.left = crack.width * fila
+                crack.top  = crack.height * columna
+                crack.draw()
+                
+            elif (mapa[fila][columna] == 3): # piso (c/ huesitos)
+                # huesos
+                huesos.left = huesos.width * fila
+                huesos.top  = huesos.height * columna
+                huesos.draw()
+                
+def draw():
+    dibujar_mapa(mapa)
