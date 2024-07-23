00bb44985a3c8a68bd2db6f5f697510eb3cbd97e
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-07-22 22:06:15 -0300

    Version actual: [M7.L1: Actividad 5/9]
    Dibujamos personaje y mostramos atributos

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #pgzero
 
 """
-Version actual: [M7.L1: Actividad 4/9]
+Version actual: [M7.L1: Actividad 5/9]
 packs de assets: https://kenney.nl/assets/series:Tiny?sort=update
 """
 
@@ -22,6 +22,12 @@ HEIGHT = celda.height * size_h
 
 TITLE = "Mazmorras" # Título de la ventana de juego
 FPS = 30 # Número de fotogramas por segundo
+
+# PERSONAJE:
+
+personaje = Actor("stand")
+personaje.salud = 100
+personaje.ataque = 5
 
 mapa = [[0, 0, 0, 0, 0, 0, 0],
         [0, 1, 2, 1, 3, 1, 0],
@@ -74,4 +80,8 @@ def dibujar_mapa(mapa):
                 huesos.draw()
                 
 def draw():
-    dibujar_mapa(mapa)
+  dibujar_mapa(mapa)
+  personaje.draw()
+
+  screen.draw.text(("PS:" + str(personaje.salud)), midright=((WIDTH - 15), 14), color = 'white', fontsize = 16)
+  screen.draw.text(("ATK:" + str(personaje.ataque)), midright=((WIDTH - 15), 36), color = 'white', fontsize = 16)
